5ce103b100638fd489e69e280315164d1172337d
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2020-06-17 14:56:09 +0100

    Sorting names using Lambda

---
+++ SortingNames.py
@@ -0,0 +1,3 @@
+names = ["Rishikesh", "aman", "Ajay", "Hemkesh", "sandeep", "Darshan", "Virendra", "Shwetabh"];
+namesSorted = sorted(names, key=lambda name:name.lower());
+print("Names Sorted ", namesSorted)
