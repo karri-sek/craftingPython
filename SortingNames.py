d2574661386c8b592a3e59a32b13e0e7c6a0d96b
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2020-06-17 14:58:44 +0100

    Sorting names using Lambda

--- SortingNames.py
+++ SortingNames.py
@@ -1,3 +1,8 @@
 names = ["Rishikesh", "aman", "Ajay", "Hemkesh", "sandeep", "Darshan", "Virendra", "Shwetabh"];
 namesSorted = sorted(names, key=lambda name:name.lower());
 print("Names Sorted ", namesSorted)
+
+#Sorting dictinaries
+xs = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
+sortedDic = sorted(xs.items(), key=lambda x:x[1])
+print("Sorted dictonaries ", sortedDic)
