a0ff052ed497105070c09d3e2e0f58ef96920a3c
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2020-06-17 21:40:56 +0100

    Assert

--- SortingNames.py
+++ SortingNames.py
@@ -10,3 +10,14 @@ sortedDic = sorted(xs.items(), key=lambda x:x[1])
 print("Sorted dictonaries ", sortedDic)
 sortedOperator = sorted(xs.items(), key=operator.itemgetter(1))
 print(" Sorted using operator ", sortedOperator)
+
+# assert in python
+
+def apply_discount(product, discount):
+    price = int(product['price'] * (1.0 - discount))
+    assert 0 <= price <=product['price']
+    return price
+
+shoes = {'name': 'reebok', 'price': 1400}
+
+print(" discounted price",apply_discount(shoes, .70));
