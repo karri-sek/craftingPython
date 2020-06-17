0e15b62a23033a981300df1f339a3d5e4ad2f7e8
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2020-06-17 15:27:50 +0100

    using operator

--- SortingNames.py
+++ SortingNames.py
@@ -1,3 +1,5 @@
+import operator
+
 names = ["Rishikesh", "aman", "Ajay", "Hemkesh", "sandeep", "Darshan", "Virendra", "Shwetabh"];
 namesSorted = sorted(names, key=lambda name:name.lower());
 print("Names Sorted ", namesSorted)
@@ -6,3 +8,5 @@ print("Names Sorted ", namesSorted)
 xs = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
 sortedDic = sorted(xs.items(), key=lambda x:x[1])
 print("Sorted dictonaries ", sortedDic)
+sortedOperator = sorted(xs.items(), key=operator.itemgetter(1))
+print(" Sorted using operator ", sortedOperator)
